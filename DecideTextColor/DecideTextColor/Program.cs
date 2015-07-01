0c4c17f99803a5de5c1be1a746a6d36428ee6b51
Yiding <yiding.cdm.depaul@hotmail.com>
2015-07-01 17:07:32 -0500

    Init

---
+++ DecideTextColor/DecideTextColor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DecideTextColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (Console.ReadLine() != "quit")
+            {
+                Console.WriteLine("Please enter the hex color: \n");
+                var hexColor = Console.ReadLine();
+                var textColor = getContrastYIQ(hexColor);
+                Console.WriteLine("The text color should be {0}", textColor);
+            }
+        }
+
+        private static string getContrastYIQ(string hexColor)
+        {
+            if (hexColor.Length != 6) return "Length needs to be 6";
+
+            var r = Convert.ToInt32(hexColor.Substring(0, 2), 16);
+            var g = Convert.ToInt32(hexColor.Substring(2, 2), 16);
+            var b = Convert.ToInt32(hexColor.Substring(4, 2), 16);
+
+            var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+
+            return yiq > 128 ? "Black" : "White";
+        }
+    }
+}
