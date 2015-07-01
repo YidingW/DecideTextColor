a68e93cb7f71b8deb475271a3594927d507d5055
Yiding <yiding.cdm.depaul@hotmail.com>
2015-07-01 17:15:38 -0500

    Error handling; Optimize While loop

--- DecideTextColor/DecideTextColor/Program.cs
+++ DecideTextColor/DecideTextColor/Program.cs
@@ -6,26 +6,34 @@ namespace DecideTextColor
     {
         static void Main(string[] args)
         {
-            while (Console.ReadLine() != "quit")
+            var colorInput = "";
+            while (colorInput != "quit")
             {
                 Console.WriteLine("Please enter the hex color: \n");
-                var hexColor = Console.ReadLine();
-                var textColor = getContrastYIQ(hexColor);
-                Console.WriteLine("The text color should be {0}", textColor);
+                colorInput = Console.ReadLine();
+                var textColor = getContrastYIQ(colorInput);
+                Console.WriteLine("The text color should be {0}. \n", textColor);
             }
         }
 
         private static string getContrastYIQ(string hexColor)
         {
             if (hexColor.Length != 6) return "Length needs to be 6";
+            try
+            {
+                var r = Convert.ToInt32(hexColor.Substring(0, 2), 16);
+                var g = Convert.ToInt32(hexColor.Substring(2, 2), 16);
+                var b = Convert.ToInt32(hexColor.Substring(4, 2), 16);
 
-            var r = Convert.ToInt32(hexColor.Substring(0, 2), 16);
-            var g = Convert.ToInt32(hexColor.Substring(2, 2), 16);
-            var b = Convert.ToInt32(hexColor.Substring(4, 2), 16);
+                var yiq = (r * 299 + g * 587 + b * 114) / 1000;
 
-            var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+                return yiq > 128 ? "Black" : "White";
+            }
+            catch (Exception)
+            {
+                return "Invalid Input!";
+            }
 
-            return yiq > 128 ? "Black" : "White";
         }
     }
 }
